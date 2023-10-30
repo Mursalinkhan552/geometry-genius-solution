16376558f21031b48ad87618dca00c44f4c7e27e
unknown <mursalinkhan552@gmail.com>
2023-10-30 23:18:24 +0600

    Some Error Handle Solved

--- js/areaCalculation.js
+++ js/areaCalculation.js
@@ -8,25 +8,34 @@ function field(elementId) {
     const baseFieldFixed = parseFloat(baseFieldElementString);
     if (isNaN(baseFieldFixed)) {
         alert('Please Provide a Correct value??');
-        return;
+       return;
     }
-    
+     
     else {
         const baseField = baseFieldFixed.toFixed(2)
         return baseField;
     }
 }
-function setField(id, value) {
+function setField(id1, id2, value) {
         
-    const triangleResultElement = document.getElementById(id);
-    triangleResultElement.innerText = value;
+    const triangleResultElement = document.getElementById(id1);
+    const triangleText = document.getElementById(id2);
+    
+    if(isNaN(value)){
+        triangleText.style.display = 'none';
+    }
+    
+    else{
+        triangleResultElement.innerText = value;
+        triangleText.style.display = 'block';
+    }
 }
 
 
-function getText(id) {
-    const triangleText = document.getElementById(id);
-    triangleText.style.display = 'block';
-}
+// function getText(id) {
+//     const triangleText = document.getElementById(id);
+//     triangleText.style.display = 'block';
+// }
 
 
 
@@ -37,9 +46,9 @@ document.getElementById('btn-triangle').addEventListener('click', function () {
     const heightField = field('height-field');
 
     const triangleTheory = 0.5 * baseField * heightField;
-    setField('triangle-result', triangleTheory)
+    setField('triangle-result','triangle-text', triangleTheory)
 
-    getText('triangle-text')
+    // getText('triangle-text')
 
 })
 
@@ -49,9 +58,9 @@ document.getElementById('btn-rectangle').addEventListener('click', function () {
     const lengthField = field('length-field');
 
     const rectangleTheory = weightField * lengthField;
-    setField('rectangle-result', rectangleTheory)
+    setField('rectangle-result','rectangle-text', rectangleTheory)
 
-    getText('rectangle-text')
+    // getText('rectangle-text')
 })
 
 document.getElementById('btn-parallelogram').addEventListener('click', function () {
@@ -61,9 +70,9 @@ document.getElementById('btn-parallelogram').addEventListener('click', function 
 
     const parallelogramTheory = baseField * heightField;
 
-    setField('parallelogram-result', parallelogramTheory)
+    setField('parallelogram-result','parallelogram-text', parallelogramTheory)
 
-    getText('parallelogram-text')
+    // getText('parallelogram-text')
 
 })
 
@@ -71,22 +80,22 @@ document.getElementById('btn-rhombus').addEventListener('click', function () {
     const firstDistance = field('first-distance-field');
     const secondDistance = field('second-distance-field');
     const rhombusTheory = firstDistance * secondDistance;
-    setField('rhombus-result', rhombusTheory)
-    getText('rhombus-text');
+    setField('rhombus-result', 'rhombus-text', rhombusTheory)
+    // getText('rhombus-text');
 })
 
 document.getElementById('btn-pentagon').addEventListener('click', function () {
     const pentagonDistance = field('pentagon-field');
     const pentagonBase = field('pentagon-base-field');
     const pentagonTheory = 0.5 * pentagonDistance * pentagonBase;
-    setField('pentagon-result', pentagonTheory)
-    getText('pentagon-text');
+    setField('pentagon-result', 'pentagon-text', pentagonTheory)
+    // getText('pentagon-text');
 })
 
 document.getElementById('btn-ellipse').addEventListener('click', function () {
     const ellipseAreaField = field('ellipse-area-field');
     const ellipseBaseField = field('ellipse-base-field')
     const ellipseTheory = 3.14 * ellipseAreaField * ellipseBaseField;
-    setField('ellipse-result', ellipseTheory)
-    getText('ellipse-text')
+    setField('ellipse-result', 'ellipse-text', ellipseTheory)
+    // getText('ellipse-text')
 })
